8e843b0cb0b798cbc2d8c1da72b5842d74099c9b
Alexey I <ialse@ya.ru>
2021-02-26 19:03:15 +0300

    доделал адаптив для попап меню и для страницы 404, сделал открытие-закрытие попапа

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,13 +4,15 @@ import accountPic from '../../images/account.svg';
 import './Navigation.css';
 
 function Navigation() {
-    function handleClick(params) {
+
+    /*Временно, чтобы работало открытие меню*/
+    function handleClick() {
         document.querySelector('.popup').classList.add('popup_opened');
     }
 
     return (
         <nav className="navigation">
-            <Link to="/movies" className="navigation__link">Фильмы</Link>
+            <Link to="/movies" className="navigation__link" >Фильмы</Link>
             <Link to="/saved-movies" className="navigation__link">Сохранённые фильмы</Link>
             <Link to="/profile" className="navigation__link-account">
                 <span className="navigation__link-text">Аккаунт</span>
--- src/components/PopupMenu/PopupMenu.js
+++ src/components/PopupMenu/PopupMenu.js
@@ -1,21 +1,28 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import accountPic from '../../images/account.svg';
-import btnClose from '../../images/button-close.svg';
 
 import './PopupMenu.css';
 
 function PopupMenu() {
+    /*Временно, чтобы работало подчеркивание ссылки*/
+    const location = useLocation().pathname;
+
+    /*Временно, чтобы работало закрытие меню*/
+    function handleClick() {
+        document.querySelector('.popup').classList.remove('popup_opened');
+    }
+
     return (
         <nav className="popup">
             <div className="popup__container">
-                <Link to="/" className="popup__link">Главная</Link>
-                <Link to="/movies" className="popup__link">Фильмы</Link>
-                <Link to="/saved-movies" className="popup__link">Сохранённые фильмы</Link>
-                <Link to="/profile" className="popup__link-account">
+                <Link to="/" className="popup__link" onClick={handleClick}>Главная</Link>
+                <Link to="/movies" className={`popup__link ${location === '/movies' && 'popup__link_current'}`} onClick={handleClick}>Фильмы</Link>
+                <Link to="/saved-movies" className={`popup__link ${location === '/saved-movies' && 'popup__link_current'}`} onClick={handleClick}>Сохранённые фильмы</Link>
+                <Link to="/profile" className="popup__link-account" onClick={handleClick}>
                     <span className="popup__link-text">Аккаунт</span>
                     <img className="popup__pic" alt="Аккаунт" src={accountPic}></img>
                 </Link>
-                <button className="popup__btn-close" />
+                <button className="popup__btn-close" onClick={handleClick} />
             </div>
         </nav>
     );
