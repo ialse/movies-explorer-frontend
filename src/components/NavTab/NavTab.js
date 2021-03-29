505db6a356529a8d2bfbed799974a44848114148
Aleksey <ialse@ya.ru>
2021-03-29 17:20:03 +0300

    реализовал плавный скролл

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -1,10 +1,23 @@
-import "./NavTab.css";
+import './NavTab.css';
+import { Link, animateScroll as scroll, scroller } from 'react-scroll';
 
 function NavTab() {
   return (
-    <a href="#about-project" className="nav-link">
+    <a
+      onClick={() =>
+        scroller.scrollTo('about-project', {
+          duration: 500,
+          smooth: 'easeInQuad',
+        })
+      }
+      className="nav-link"
+    >
       <button className="nav-link__button">Узнать больше</button>
     </a>
+
+    /*<Link to="about-project" className="nav-link" smooth={easeInOutQuint}>
+      <button className="nav-link__button">Узнать больше</button>
+    </Link>*/
   );
 }
 
